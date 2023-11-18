fe04ac7a594dc063d03f75c5f673912281393da2
nizirux <nizar.azzouzi@etu.univ-amu.fr>
2023-11-18 22:28:36 +0100

    started implementing machine methods

--- src/machine/Machine.java
+++ src/machine/Machine.java
@@ -6,20 +6,21 @@ import java.util.List;
 
 import beans.Account;
 import beans.Film;
+import beans.User;
 import dao.tools.Session;
 import facade.bd.FacadeBd;
 
 public class Machine {
 	
-	FacadeBd facadeBd = new FacadeBd();
-	Account account = null;
-
+	private static FacadeBd facadeBd = new FacadeBd();
+	private Account account;
+	
 	public static void main(String[] args) {
 		
 		Session session = new Session(false);
-		
+		//Account myAccount = new beans.NormalAccount();
 		Machine machine = new Machine();
-	
+
 		try {
 			session.open();
 			machine.getAllFilms();
@@ -34,13 +35,43 @@ public class Machine {
 
 		}
 	}
-	
+
+	public boolean createUserAccount(User user, String email, String password){
+		return facadeBd.createUserAccount(user, email, password);
+	}
+
+	public boolean subscribeToService(Account account){
+		beans.SubscriberAccount subAccount = (beans.SubscriberAccount) facadeBd.subscribeToService(account);
+		if (subAccount != null) {
+			this.account = subAccount;
+			return true;
+		}
+		return false;	
+	}
+
 	public void getAllFilms() {
-		 ArrayList<Film> films = (ArrayList<Film>) facadeBd.getAllFilms(account);
-		 		 
-		 for (Film f : films) {
-			 System.out.println(f);
-		 }
+		ArrayList<Film> films = (ArrayList<Film>) facadeBd.getAllFilms(account); 		 
+		for (Film film : films) {
+			System.out.println(film);
+		}
     }
 
-}
+	public void getAccountBannedCategories(Account account){
+		ArrayList<beans.Category> bannedCategories = (ArrayList<beans.Category>) facadeBd.getBannedCategories(account);
+		String output = "";
+		int i = 0;
+		for (beans.Category c : bannedCategories){
+			output += c + ", ";
+			i++;
+			if (i == bannedCategories.size() - 1){
+				output += c;
+			}
+ 		}
+		System.out.println(account.getUser().getFirstName()+ "'s banned categories: " + output);
+	}
+
+	public void getAccountRentalHistory(Account account){
+		//TO DO we dont have a function for this in the dao
+	}
+
+}		
